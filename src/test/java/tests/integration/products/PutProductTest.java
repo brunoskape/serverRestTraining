86fc963b58d2c4769847ceb27c46a97f74ad2674
skape__@hotmail.com <Tel27200745>
2022-03-14 16:18:12 -0300

    Add PutProductTest.

--- src/test/java/tests/integration/products/PutProductTest.java
+++ src/test/java/tests/integration/products/PutProductTest.java
@@ -1,11 +1,13 @@
 package tests.integration.products;
 
+import com.github.javafaker.Faker;
 import tests.factory.Product;
 import io.restassured.http.ContentType;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import tests.utils.BaseTest;
 import tests.utils.LoginUtil;
+import tests.utils.ProductUtil;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -15,41 +17,40 @@ import static org.hamcrest.Matchers.is;
 public class PutProductTest extends BaseTest {
 
     LoginUtil login = new LoginUtil();
+    ProductUtil product = new ProductUtil();
 
 
-    @BeforeTest
-    public void createProductAndGetToken() {
-        Product product = new Product("mouse pad gamer199", "120", "mouse pad gamer com rgb", "3");
+//melhorar
+     @Test
+    public void PutProduct(){
 
-        String productId =
-                given()
-                       .headers("Authorization", login.getToken())
-                       .body(product)
-                       .when()
-                       .post("/produtos")
-                       .then()
-                       .statusCode(201)
-                       .body("message", is("Cadastro realizado com sucesso"))
-                       .extract().path("_id");
+         Product product = new Product("mouse pad altput", 120, "mouse pad gamer com rgb",3);
 
-    }
+         String productId =
+                 given()
+                         .headers("Authorization", login.getToken())
+                         .body(product)
+                         .when()
+                         .post("/produtos")
+                         .then()
+                         .statusCode(201)
+                         .body("message", is("Cadastro realizado com sucesso"))
+                         .extract().path("_id");
 
-    @Test
-    public void updateProductTest(){
-
-        Product product = new Product("mouse pad gamer199", "120", "mouse pad gamer com rgb","3");
-
-        given()
-                .contentType(ContentType.JSON)
-                .headers("Authorization", login.getToken())
-                .body(product)
-                .when()
-                //.put("/produtos" + productId)
-                .then()
-                .statusCode(200)
-                .body("message", equalTo("Registro alterado com sucesso"));
+         given()
+                 .contentType(ContentType.JSON)
+                 .headers("Authorization", login.getToken())
+                 .body(product)
+                 .when()
+                 .put("/produtos/" + productId)
+                 .then()
+                 .statusCode(200)
+                 .body("message", equalTo("Registro alterado com sucesso"));
 
 
-    }
+     }
+
 
 }
+
+
